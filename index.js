e9f699a21b2c2be333a0467513688b48546fe865
Whitelakeouf <belozerov.dmitry@itglobal.ru>
2022-04-29 14:50:29 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const userRoute = require('./routes/user');
+
+dotenv.config();
+
+mongoose
+    .connect(process.env.MONGO_URL)
+    .then(() => console.log('DBConnection successfull!'))
+    .catch((err) => console.log(err));
+
+app.use(express.json());
+app.use('/api/user', userRoute);
+
+app.listen(process.env.PORT || 5000, () => {
+    console.log('Backend server is running!');
+});
